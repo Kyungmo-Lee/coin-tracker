2c9fb09d9873912e48bb2faed0226da5de0d2f2c
Kyungmo-Lee <leekyungmo12@gmail.com>
2023-01-04 10:07:20 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Btn = styled.button`
+  background-color: ${(props) => props.theme.btnColor};
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <H1>protected</H1>
+        <Btn>is</Btn>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
